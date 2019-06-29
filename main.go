d20f624941c45b75e07a63c2abd36bd22907ede4
Tyler Tomlinson <tylerjtomlinson@gmail.com>
2019-06-29 16:24:31 -0500

    Request response

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
-package currentStable
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintln(w, r.RequestURI)
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", index).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
